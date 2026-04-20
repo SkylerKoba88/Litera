beb173e60ff512485fc0dfb9f5807ee1de46fb5e
SkylerKoba88 <sfk5879@psu.edu>
2026-04-20 18:17:45 -0400

    production fix

--- litera-db.session.sql
+++ litera-db.session.sql
@@ -1,2 +1,3 @@
-ALTER TABLE litera.communities
-  ADD COLUMN rules TEXT DEFAULT NULL;
+UPDATE litera.communities
+SET thumbnail_url = 'https://www.baltimoremagazine.com/wp-content/uploads/2026/02/wuthering-heights-header-1-1200x675.jpg'
+WHERE id = 1;
